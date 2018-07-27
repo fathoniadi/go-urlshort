0adc1b511dcb13129617b8f9eac56ca0f537dc92
fathoniadi <fathoni.adi@gmail.com>
2018-07-27 18:14:50 +0700

    oke

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
@@ -22,5 +25,27 @@ func main() {
 	app.Mount("/api", api)
 	app.Mount("/", web)
 
+	workDir, _ := os.Getwd()
+	filesDir := filepath.Join(workDir, "static")
+	FileServer(app, "/public/static", http.Dir(filesDir))
+
 	http.ListenAndServe(":3000", app)
+}
+
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit URL parameters.")
+	}
+
+	fs := http.StripPrefix(path, http.FileServer(root))
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fs.ServeHTTP(w, r)
+	}))
 }
