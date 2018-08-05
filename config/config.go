91d83365aafe481e678f153c26e83136945e1731
fathoniadi <fathoni.adi@gmail.com>
2018-08-05 22:56:58 +0700

    perbaiki .env

--- config/config.go
+++ config/config.go
@@ -8,10 +8,11 @@ import (
 )
 
 type Config struct {
-	database_host     string
-	database_port     int
-	database_username string
-	database_password string
+	Database_host     string
+	Database_port     int
+	Database_username string
+	Database_name     string
+	Database_password string
 }
 
 func Load() Config {
@@ -23,20 +24,24 @@ func Load() Config {
 
 	c := Config{}
 
-	if c.database_host = os.Getenv("DATABASE_HOST"); c.database_host == "" {
-		c.database_host = "localhost"
+	if c.Database_host = os.Getenv("DATABASE_HOST"); c.Database_host == "" {
+		c.Database_host = "localhost"
 	}
 
-	if c.database_port, _ = strconv.Atoi(os.Getenv("DATABASE_PORT")); c.database_port == 0 {
-		c.database_port = 27017
+	if c.Database_port, _ = strconv.Atoi(os.Getenv("DATABASE_PORT")); c.Database_port == 0 {
+		c.Database_port = 27017
 	}
 
-	if c.database_username = os.Getenv("DATABASE_USERNAME"); c.database_username == "" {
-		c.database_username = ""
+	if c.Database_username = os.Getenv("DATABASE_USERNAME"); c.Database_username == "" {
+		c.Database_username = ""
 	}
 
-	if c.database_password = os.Getenv("DATABASE_PASSWORD"); c.database_password == "" {
-		c.database_password = ""
+	if c.Database_password = os.Getenv("DATABASE_PASSWORD"); c.Database_password == "" {
+		c.Database_password = ""
+	}
+
+	if c.Database_name = os.Getenv("DATABASE_Name"); c.Database_name == "" {
+		c.Database_name = ""
 	}
 
 	return c
--- core/Database.go
+++ core/Database.go
@@ -1,17 +1,26 @@
 package core
 
 import (
-	"fmt"
 	config "github.com/fathoniadi/go-urlshort/config"
 	"gopkg.in/mgo.v2"
+	"strconv"
+	"time"
 )
 
 func DBConnect() *mgo.Session {
 	db_config := config.Load()
-	fmt.Println(db_config)
-	db, err := mgo.Dial("localhost")
+
+	mongoDBDialInfo := &mgo.DialInfo{
+		Addrs:    []string{db_config.Database_host + ":" + strconv.Itoa(db_config.Database_port)},
+		Timeout:  60 * time.Second,
+		Database: db_config.Database_name,
+		Username: db_config.Database_username,
+		Password: db_config.Database_password,
+	}
+
+	db, err := mgo.DialWithInfo(mongoDBDialInfo)
 	if err != nil {
-		fmt.Println("error connect")
+		panic(err)
 	}
 
 	return db
